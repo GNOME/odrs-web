d35f2a321e5d24272dfe4291de7a5d614955a107
Andrea Veri <averi@redhat.com>
2018-12-17 12:50:35 +0100

    Temp comment (debugging)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-pwd
-sed -i "s/replace_odrs_secret/${ODRS_REVIEWS_SECRET}/" /etc/httpd/sites.d/odrs.gnome.org.conf
-sed -i "s/replace_odrs_username/${MYSQL_DB_USERNAME}/" /etc/httpd/sites.d/odrs.gnome.org.conf
-sed -i "s/replace_odrs_password/${MYSQL_DB_PASSWORD}/" /etc/httpd/sites.d/odrs.gnome.org.conf
-sed -i "s/replace_odrs_host/${MYSQL_DB_HOST}/" /etc/httpd/sites.d/odrs.gnome.org.conf
+#sed -i "s/replace_odrs_secret/${ODRS_REVIEWS_SECRET}/" /etc/httpd/sites.d/odrs.gnome.org.conf
+#sed -i "s/replace_odrs_username/${MYSQL_DB_USERNAME}/" /etc/httpd/sites.d/odrs.gnome.org.conf
+#sed -i "s/replace_odrs_password/${MYSQL_DB_PASSWORD}/" /etc/httpd/sites.d/odrs.gnome.org.conf
+#sed -i "s/replace_odrs_host/${MYSQL_DB_HOST}/" /etc/httpd/sites.d/odrs.gnome.org.conf
 
-while true; do python /opt/app-root/src/cron.py ratings /opt/app-root/src/app/static/ratings.json ; sleep 43200 ; done &
+#while true; do python /opt/app-root/src/cron.py ratings /opt/app-root/src/app/static/ratings.json ; sleep 43200 ; done &
 
 exec /usr/bin/run-httpd
