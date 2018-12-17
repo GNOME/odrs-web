79477c996d03eba61ace435cf928bd456e97aedf
Andrea Veri <averi@redhat.com>
2018-12-17 12:45:51 +0100

    id run (debugging)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+id 
 sed -i "s/replace_odrs_secret/${ODRS_REVIEWS_SECRET}/" /etc/httpd/sites.d/odrs.gnome.org.conf
 sed -i "s/replace_odrs_username/${MYSQL_DB_USERNAME}/" /etc/httpd/sites.d/odrs.gnome.org.conf
 sed -i "s/replace_odrs_password/${MYSQL_DB_PASSWORD}/" /etc/httpd/sites.d/odrs.gnome.org.conf
