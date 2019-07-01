f4484685c0edbb036d447fc9d25faed39b686805
Richard Hughes <richard@hughsie.com>
2019-07-01 12:39:06 +0100

    trivial: Fix path

--- app_data/httpd-pre-init/entrypoint.sh
+++ app_data/httpd-pre-init/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python36 /opt/app-root/src/app_data/wsgi-scripts/odrs-apache-setup.py ${HTTPD_CONFIGURATION_PATH}/odrs.gnome.org.conf
+python36 /opt/app-root/src/wsgi-scripts/odrs-apache-setup.py ${HTTPD_CONFIGURATION_PATH}/odrs.gnome.org.conf
 
 while true; do python36 /opt/app-root/src/cron.py ratings /opt/app-root/src/odrs/static/ratings.json ; sleep 43200 ; done &
