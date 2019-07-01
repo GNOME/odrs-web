3ed6ba5614cbd26114e6fa130180d5e875e6ae5e
Richard Hughes <richard@hughsie.com>
2019-07-01 12:22:21 +0100

    Don't use sed for environ substitution

--- app_data/httpd-pre-init/entrypoint.sh
+++ app_data/httpd-pre-init/entrypoint.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-sed -i "s/replace_odrs_secret/${ODRS_REVIEWS_SECRET}/" ${HTTPD_CONFIGURATION_PATH}/odrs.gnome.org.conf 
-sed -i "s/replace_odrs_database_uri/${SQLALCHEMY_DATABASE_URI}/" ${HTTPD_CONFIGURATION_PATH}/odrs.gnome.org.conf
-
 while true; do python36 /opt/app-root/src/cron.py ratings /opt/app-root/src/odrs/static/ratings.json ; sleep 43200 ; done &
