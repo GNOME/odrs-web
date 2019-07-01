fe23ff2e2ddcb98be4f3f2c2f2f433fab76aaffd
Richard Hughes <richard@hughsie.com>
2019-07-01 13:27:42 +0100

    Automatically delete reviews marked for deletion over one month ago
    The one month buffer is a compromise defending against a moderately good hacker
    deleting all the reviews using the anon API, and GDPR provisions.

--- app_data/cron.py
+++ app_data/cron.py
@@ -7,11 +7,28 @@
 
 import json
 import sys
+import datetime
 
 from odrs import db
 
 from odrs.models import Review
 from odrs.util import _get_rating_for_app_id
+
+def _auto_delete():
+
+    since = datetime.datetime.now() - datetime.timedelta(days=31)
+    reviews = db.session.query(Review).\
+                    filter(Review.date_deleted != None).\
+                    filter(Review.date_deleted < since).\
+                    order_by(Review.date_created.asc()).\
+                    all()
+    if len(reviews) > 1000:
+        print('too many reviews to delete: {}'.format(len(reviews)))
+        return
+    print('Deleting {} reviews...'.format(len(reviews)))
+    for review in reviews:
+        db.session.delete(review)
+    db.session.commit()
 
 def _regenerate_ratings(fn):
     item = {}
@@ -31,13 +48,18 @@ def _regenerate_ratings(fn):
 
 if __name__ == '__main__':
 
-    if len(sys.argv) < 3:
-        print('Usage: %s ratings filename' % sys.argv[0])
+    if len(sys.argv) < 2:
+        print('Usage: %s ratings|auto-delete' % sys.argv[0])
         sys.exit(1)
 
     # create the ratings data
     if sys.argv[1] == 'ratings':
+        if len(sys.argv) < 3:
+            print('Usage: %s ratings filename' % sys.argv[0])
+            sys.exit(1)
         _regenerate_ratings(sys.argv[2])
+    if sys.argv[1] == 'auto-delete':
+        _auto_delete()
     else:
         print("cron mode %s not known" % sys.argv[1])
         sys.exit(1)
