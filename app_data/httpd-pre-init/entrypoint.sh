f21af73527dbd723199a9646eaa3c93251a19cd6
Richard Hughes <richard@hughsie.com>
2020-04-29 17:35:46 +0100

    trivial: Use a more standard Python binary name

--- app_data/httpd-pre-init/entrypoint.sh
+++ app_data/httpd-pre-init/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python36 /opt/app-root/src/wsgi-scripts/odrs-apache-setup.py ${HTTPD_CONFIGURATION_PATH}/odrs.gnome.org.conf
+python3 /opt/app-root/src/wsgi-scripts/odrs-apache-setup.py ${HTTPD_CONFIGURATION_PATH}/odrs.gnome.org.conf
 
-while true; do python36 /opt/app-root/src/cron.py ratings /opt/app-root/src/odrs/static/ratings.json ; sleep 43200 ; done &
-while true; do python36 /opt/app-root/src/cron.py fsck ; sleep 43200 ; done &
+while true; do python3 /opt/app-root/src/cron.py ratings /opt/app-root/src/odrs/static/ratings.json ; sleep 43200 ; done &
+while true; do python3 /opt/app-root/src/cron.py fsck ; sleep 43200 ; done &
