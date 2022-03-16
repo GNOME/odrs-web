25cb4bf9a65ab4f854c226927471c35b3fbf2515
Richard Hughes <richard@hughsie.com>
2022-03-16 19:45:47 +0000

    Another gzip -k fix for RHEL 7

--- app_data/httpd-pre-init/entrypoint.sh
+++ app_data/httpd-pre-init/entrypoint.sh
@@ -5,7 +5,9 @@ python3 /opt/app-root/src/wsgi-scripts/odrs-apache-setup.py ${HTTPD_CONFIGURATIO
 while true; do
   python3 /opt/app-root/src/cron.py ratings /opt/app-root/src/odrs/static/ratings.json
   sleep 43200
-  gzip -k /opt/app-root/src/odrs/static/ratings.json
+  cp /opt/app-root/src/odrs/static/ratings.json /opt/app-root/src/odrs/static/ratings.json.bak
+  gzip /opt/app-root/src/odrs/static/ratings.json
+  mv /opt/app-root/src/odrs/static/ratings.json.bak /opt/app-root/src/odrs/static/ratings.json
   brotli -k /opt/app-root/src/odrs/static/ratings.json
 done &
 while true; do python3 /opt/app-root/src/cron.py fsck ; sleep 43200 ; done &
